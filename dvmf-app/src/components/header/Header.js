f8ed3eaa415a2eaf3464aadc7c78e9629bc8d8f4
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-06-06 17:53:38 +0200

    WIP creating hamburger menu

--- dvmf-app/src/components/header/Header.js
+++ dvmf-app/src/components/header/Header.js
@@ -7,6 +7,20 @@ function Header(){
             <figure>
                 <img src={logo} alt="" />
             </figure>
+
+            <div class="hamburger-menu">
+                <input id="menu__toggle" type="checkbox" />
+                <label class="menu__btn" for="menu__toggle">
+                <span></span>
+                </label>
+
+                <ul class="menu__box">
+                    <li><a class="menu__item" href="#">Home</a></li>
+                    <li><a class="menu__item" href="#">Portfolio</a></li>
+                    <li><a class="menu__item" href="#">About me</a></li>
+                    <li><a class="menu__item" href="#">Contact</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
--- dvmf-app/src/components/home-item/HomeItem.js
+++ dvmf-app/src/components/home-item/HomeItem.js
@@ -2,16 +2,14 @@ import "./home-item.css";
 import HomeBackground from "../home-background/HomeBackground";
 
 function HomeItem({serviceInfo}){
+
     return (
         <div className="home-item-container">
             <HomeBackground className="home-background" />
             <div className='home-content-container'>
-                <div className='yellow-bar'></div>
+            {setBarHeight(serviceInfo.fullScreen)}
                 <div className='home-content'>
-                    <div className="home-content-titles">
-                        <h1 className='home-content-main-title'>{serviceInfo.service}</h1>
-                        <div className='home-content-background-title'>{serviceInfo.service}</div>
-                    </div>
+                    {setMarginTitles(serviceInfo)}
                     <p className='home-content-info'>{serviceInfo.description}</p>
                     <div className='home-content-buttons'>
                         <a className='home-content-button' href="">Portfolio <span className='icon-right_arrow_big_icon'></span></a>
@@ -19,9 +17,37 @@ function HomeItem({serviceInfo}){
                     </div>
                 </div>
             </div>
-            <span className="icon-down_arrow_small_icon home-arrow"></span>
+            {setLatestArrow(serviceInfo.service)}
         </div>
     )
+}
+
+function setBarHeight(isFull){
+    if(isFull == true){
+        return <div className='yellow-bar'></div>
+    } else {
+        return <div className='yellow-bar-small'></div>
+    }
+}
+
+function setMarginTitles(serviceInfo){
+    if(serviceInfo.fullScreen == true){
+        return <div className="home-content-titles">
+        <h1 className='home-content-main-title'>{serviceInfo.service}</h1>
+        <div className='home-content-background-title'>{serviceInfo.service}</div>
+    </div>
+    } else {
+        return <div className="home-content-titles-small">
+        <h1 className='home-content-main-title'>{serviceInfo.service}</h1>
+        <div className='home-content-background-title'>{serviceInfo.service}</div>
+    </div>
+    }
+}
+
+function setLatestArrow(service){
+    if(service !== "Web"){
+        return <span className="icon-down_arrow_small_icon home-arrow"></span>
+    }
 }
 
 export default HomeItem;
