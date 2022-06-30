7b7bfd6522facaff531966985c4c2cb492dcdd53
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-06-30 17:20:45 +0200

    Made home background dynamic and added background to moving arrow

--- dvmf-app/src/components/home-background/HomeBackground.js
+++ dvmf-app/src/components/home-background/HomeBackground.js
@@ -1,11 +1,12 @@
 import background_image from '../../assets/images/home_background_placeholder.jpg';
 import "./home-background.css";
 
-function HomeBackground () {
+function HomeBackground(props) {
+    console.log(props);
     return (
         <div>
             <figure className='home-background-figure'>
-                <img className='home-background-image' src={background_image} alt="" />
+                <img className='home-background-image' src={props.data} alt="" />
             </figure>
         </div>
     );
