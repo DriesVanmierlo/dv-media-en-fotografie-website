7b3c288732886f6ef8974104e49be8f428f92e90
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-09-30 13:59:46 +0200

    Added Bal Tropical and Beauty and More video's to the side

--- dvmf-app/src/pages/Email.js
+++ dvmf-app/src/pages/Email.js
@@ -15,7 +15,8 @@ function Email () {
                 <h1 className='email-title'>Blijf op de hoogte wanneer je foto&#39;s online komen!</h1>
                 <p className='email-text'>Vul je e-mailadres in waarmee je op de hoogte gehouden wilt worden. Ik laat je weten wanneer de foto&#39;s beschikbaar zijn!</p>
                 <div className='email-form'>
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc9o42osGeqNADDm1umlbmqapV3GQF4rKDhnw7SNCzbNyOE6g/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Laden…</iframe>
+                    <h2 className='email-title'>Momenteel staat er geen formulier open</h2>
+                    {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc9o42osGeqNADDm1umlbmqapV3GQF4rKDhnw7SNCzbNyOE6g/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Laden…</iframe> */}
                 </div>
             </div>
         </>
